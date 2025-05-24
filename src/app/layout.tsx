b8f818a78471ab52869462ea043d60fcd9beefc8
unknown <larrieta@correo.unimet.edu.ve>
2025-05-24 19:15:43 -0400

    Implementación de archivos
    Cree archivos para hacer el footer, estoy viendo como organizarlo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,14 @@ export default function DashboardLayout({
         <body>
           {/* Layout UI */}
           {/* Place children where you want to render a page or nested layout */}
-          <main><div>Header</div>{children}<div>Footer</div></main>
+          <main>
+            <div>
+              Header
+            </div>
+             {children}
+            <div>
+              Footer
+            </div></main>
         </body>
       </html>
     )
